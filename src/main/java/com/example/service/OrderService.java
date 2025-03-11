f4bb28b4224dfe47d172940217d50ba6a38d7c59
RANASABRY <ranasabryy9@gmail.com>
2025-03-11 13:07:36 +0200

    service

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,4 +1,41 @@
 package com.example.service;
 
-public class OrderService {
+import com.example.model.Order;
+import com.example.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService  extends  MainService<Order>{
+    private final OrderRepository orderRepository ;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+    public ArrayList<Order> getOrders(){
+        return orderRepository.getOrders();
+    }
+
+    public Order getOrderById(UUID orderId){
+        return orderRepository.getOrderById(orderId);
+    }
+
+    public List<Order> getOrdersByUserId(UUID userId) {
+        return orderRepository.getOrdersByUserId(userId);
+    }
+
+    public void addOrder(Order order) {
+        orderRepository.addOrder(order);
+    }
+
+
+    public void deleteOrderById(UUID orderId) throws IllegalArgumentException{
+        orderRepository.deleteOrderById(orderId);
+    }
+
 }
